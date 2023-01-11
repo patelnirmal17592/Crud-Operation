4d4ef572f6a10063c8e91cfd30c843cc122a3049
Nirmal Patel <patelnirmal17592@gmail.com>
2023-01-11 16:24:37 +1300

    Database connection is done

--- Crud Operation/RepositoryLayer/CrudOperationRL.cs
+++ Crud Operation/RepositoryLayer/CrudOperationRL.cs
@@ -1,12 +1,53 @@
 ﻿using Crud_Operation.CommonLayer.Model;
+using System.Data.SqlClient;
 
 namespace Crud_Operation.RepositoryLayer
 {
     public class CrudOperationRL : ICrudOperationRL
     {
-        public Task<CreateRecordResponse> CreateRecord(CreateRecordRequest request)
+
+        public readonly IConfiguration _configuration;
+        public readonly SqlConnection _sqlConnection;
+        public CrudOperationRL(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration= configuration;
+            _sqlConnection = new SqlConnection(_configuration["ConnectionStrings:DbSettingConnection"]);
+        }
+        public async Task<CreateRecordResponse> CreateRecord(CreateRecordRequest request)
+        {
+            CreateRecordResponse response = new CreateRecordResponse();
+            response.IsSuccess= true;
+            response.Message = "Successful";
+            try
+            {
+                string SqlQuery = "Insert Into CrudOperationTable (UserName, Age) values (@UserName, @Age)";
+                using (SqlCommand sqlCommand = new SqlCommand(SqlQuery, _sqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.Text;
+                    sqlCommand.CommandTimeout = 180;
+                    sqlCommand.Parameters.AddWithValue("@UserName", request.UserName);
+                    sqlCommand.Parameters.AddWithValue("@Age", request.Age);
+                    _sqlConnection.Open();
+                    int Status = await sqlCommand.ExecuteNonQueryAsync();
+
+                    if(Status <= 0)
+                    {
+                        response.IsSuccess = false;
+                        response.Message = "Something Went Wrong";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+                _sqlConnection.Close();
+            }
+
+            return response;
         }
     }
 }
